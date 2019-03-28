5fab3b935ee51a54f1394c0da0a6e774d93b61bd
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-03-28 16:03:28 -0700

    Added contact page. PHP form works with firefox.

---
+++ contact.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Fun Bus Travel Agency - Solution</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>   
+  <div class="container home">
+  <header>
+    <h1>Fun Bus</h1>
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="#">About Us</a>
+      <a href="#">Blog</a>
+      <a href="contact.html">Contact</a>
+    </nav>
+  </header>
+    <div class="cta">
+      <img src="img/fun-bus.jpg" al= "The Fun bus logo!">
+      <h2>Welcome to the Fun Bus!</h2>
+      <p>Traveling expedition modern, clean webdesign blogger clean website them website modern. Design pretty design, travelblogger adventure Wordpress wanderlust theme blogger website expedition theme travelblogger. Adventure fun traveler pretty design website expedition.</p>
+    </div>
+    <h2>Thanks for signing up for a extravadecadent trip!</h2>
+  </div><!-- container -->
+
+  <footer class="footer">
+    <p>Copyright Fun Bus 2018</p>
+  </footer>
+
+</body>
+</html>
